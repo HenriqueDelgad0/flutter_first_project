5da9ebd10a1f119b91ac65679f05a47f3c88d439
Henrique <henrique.delgado@warren.com.br>
2022-09-12 14:33:46 -0300

    add app main color

---
+++ lib/ds/atoms/Color.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+MaterialColor appMainColor = MaterialColor(0xFFE02B57, color);
+
+Map<int, Color> color =
+{
+      50: const Color.fromRGBO(224, 43, 87, 0.34),
+      100: const Color.fromRGBO(224, 43, 87, .2),
+      200: const Color.fromRGBO(224, 43, 87, .3),
+      300: const Color.fromRGBO(224, 43, 87, .4),
+      400: const Color.fromRGBO(224, 43, 87, .5),
+      500: const Color.fromRGBO(224, 43, 87, .6),
+      600: const Color.fromRGBO(224, 43, 87, .7),
+      700: const Color.fromRGBO(224, 43, 87, .8),
+      800: const Color.fromRGBO(224, 43, 87, .9),
+      900: const Color.fromRGBO(224, 43, 87, 1),
+};
